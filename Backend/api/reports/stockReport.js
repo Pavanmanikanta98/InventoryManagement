86770b2c71d131bc2a1175d941604fad38f9d6f7
pavan <pavanmanikanta98@gmail.com>
2024-07-03 11:27:39 +0530

    report

--- Backend/api/reports/stockReport.js
+++ Backend/api/reports/stockReport.js
@@ -5,6 +5,21 @@ const router = express.Router();
 
 //All chem list
 
+
+router.get('/', async(req, res )=>{
+try {
+
+
+    
+    
+} catch (err) {
+    console.log(err.message);
+
+    return res.status(500).json({message:"server side error"});
+}
+
+})
+
 //search by chem
 
 
